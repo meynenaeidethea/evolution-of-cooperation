f81adf6b8c6cfd8b81ee624ab09f4be194dd0e18
meynenaeidethea <meynenaeidethea@gmail.com>
2026-09-25 02:53:52 +0300

    build: remove unused sample configuration

--- simulation/build.gradle.kts
+++ simulation/build.gradle.kts
@@ -7,7 +7,7 @@
 
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
-    application
+    java
 }
 
 repositories {
@@ -20,9 +20,6 @@ dependencies {
     testImplementation(libs.junit.jupiter)
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation(libs.guava)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
@@ -30,11 +27,6 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(25)
     }
-}
-
-application {
-    // Define the main class for the application.
-    mainClass = "evolution.App"
 }
 
 tasks.named<Test>("test") {
